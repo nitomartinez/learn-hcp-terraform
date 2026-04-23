eca240714f951f5b7d660204e77454c9908485af
nitomartinez <353827+nitomartinez@users.noreply.github.com>
2026-04-23 08:57:41 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
-output "instance_hostname" {
-  description = "Private DNS name of the EC2 instance."
-  value       = aws_instance.app_server.private_dns
-}
-
-output "instance_security_group_ids" {
-  value = aws_instance.app_server.vpc_security_group_ids
-}
-
-output "instance_subnet" {
-  value = aws_instance.app_server.subnet_id
-}
-
-output "instance_ami" {
- description = "AMI used to create the EC2 instance. Edited"
- value       = aws_instance.app_server.ami
-}
+# output "instance_hostname" {
+#   description = "Private DNS name of the EC2 instance."
+#   value       = aws_instance.app_server.private_dns
+# }
+# 
+# output "instance_security_group_ids" {
+#   value = aws_instance.app_server.vpc_security_group_ids
+# }
+# 
+# output "instance_subnet" {
+#   value = aws_instance.app_server.subnet_id
+# }
+# 
+# output "instance_ami" {
+#  description = "AMI used to create the EC2 instance. Edited"
+#  value       = aws_instance.app_server.ami
+# }
